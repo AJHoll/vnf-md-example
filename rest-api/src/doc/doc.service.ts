0e1c06872926e29ebe9b79a25c6e65726392f3c4
AJHoll <anthony_j_holl@mail.ru>
2021-05-30 19:26:28 +0500

    Типизировали документ

--- rest-api/src/doc/doc.service.ts
+++ rest-api/src/doc/doc.service.ts
@@ -3,20 +3,45 @@ import { DocCard, DocList } from 'src/datasets';
 import { query } from '../controller';
 import { Request } from '../controller/classes/Request';
 
+type Doc = {
+  id: Number;
+  number: String;
+  date: Date;
+  sum: Number;
+  description: String;
+};
+
 @Injectable()
 export class DocService {
   docList: DocList = new DocList(this);
   docCard: DocCard = new DocCard(this);
 
-  async getAllDoc(): Promise<any | any[]> {
-    const data = await query(new Request(this.docList.operations.selectData));
-    return data;
+  async getAllDoc(): Promise<Doc[]> {
+    const payload = await query(
+      new Request(this.docList.operations.selectData),
+    );
+    return payload.data.map((item) => {
+      return {
+        id: item.id,
+        number: item.s_number,
+        date: item.d_date,
+        sum: item.f_sum,
+        description: item.s_description,
+      };
+    });
   }
 
-  async getOneDoc(idDoc: number): Promise<any> {
-    const data = await query(
+  async getOneDoc(idDoc: number): Promise<Doc> {
+    const payload = await query(
       new Request(this.docCard.operations.selectData, false, { id: idDoc }),
     );
-    return data;
+    const item = payload.data[0];
+    return {
+      id: item.id,
+      number: item.s_number,
+      date: item.d_date,
+      sum: item.f_sum,
+      description: item.s_description,
+    };
   }
 }
