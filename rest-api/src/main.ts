5bec59e04edd43d5913cbdb123256c5343edcab1
AJHoll <anthony_j_holl@mail.ru>
2021-05-30 18:38:13 +0500

    исправление connection и правка gitignore

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { setConnection } from './controller';
+require('dotenv').config();
 async function bootstrap() {
-  require('dotenv').config();
+  const conn = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+  };
+  await setConnection(conn);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.REST_PORT);
 }
