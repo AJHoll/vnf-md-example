a1ca78b7cea67518b9b43d7c21190bdf4ae07a8c
AJHoll <anthony_j_holl@mail.ru>
2021-05-30 18:50:13 +0500

    Доработка контроллеров для получения всего и по ид

--- rest-api/src/doc/doc.controller.ts
+++ rest-api/src/doc/doc.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { DocService } from './doc.service';
 
 @Controller('doc')
-export class DocController {}
+export class DocController {
+  constructor(private readonly docService: DocService) {}
+
+  @Get('/')
+  async getAllDoc(): Promise<any | any[]> {
+    return this.docService.getAllDoc();
+  }
+
+  @Get('/:id')
+  async getOneDoc(@Param('id') id): Promise<any> {
+    return this.docService.getOneDoc(+id);
+  }
+}
--- rest-api/src/doc/doc.service.ts
+++ rest-api/src/doc/doc.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DocService {}
+export class DocService {
+  async getAllDoc(): Promise<any | any[]> {
+    return { id: 1 };
+  }
+
+  async getOneDoc(id: number): Promise<any> {
+    return { id: id };
+  }
+}
